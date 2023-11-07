0280d9c404f995f13969d80c767ecbf0a60ea32b
daniilsmiirnov <dan_smirnov@mail.ru>
2023-11-07 23:54:21 +0300

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,13 @@ import ObjectsList from './components/ObjectsList/ObjectsList';
 // import {Container} from 'react-bootstrap'
 import AboutObject from './components/AboutObject/AboutObject';
 // import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// function App() {
+function App() {
 
-//   return (
+  return (
 
-//     // <BrowserRouter basename="/front">
-//     <BrowserRouter>
-//       <Routes>
-//         {/* <Route path="/" element={<HomePage /} />
-//         <Route path="/about" element={<AboutPage />} />
-//         <Route path="/contact" element={<ContactPage />} /> */}
-//         <Route path="/Main/" element={<ObjectsList />} />
-//         <Route path="/Main/about/" element={<AboutObject />} />
-//       </Routes>
-//     </BrowserRouter>
-
-//   );
-// };
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-    // <BrowserRouter basename="/front">
     <BrowserRouter basename="/front">
       <Routes>
         <Route path="" element={<ObjectsList />} />
@@ -41,5 +22,22 @@ root.render(
         <Route path="/Main/about/" element={<AboutObject />} />
       </Routes>
     </BrowserRouter>
-);
 
+  );
+};
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+
+// root.render(
+//     // <BrowserRouter basename="/front">
+//     <BrowserRouter basename="/front">
+//       <Routes>
+//         <Route path="" element={<ObjectsList />} />
+//         <Route path="/Main/" element={<ObjectsList />} />
+//         <Route path="/about/" element={<AboutObject />} />
+//         <Route path="/Main/about/" element={<AboutObject />} />
+//       </Routes>
+//     </BrowserRouter>
+// );
+export default App
